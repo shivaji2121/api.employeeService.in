73d448727af86e7577bc8edbe1a4a30c2700f271
shivaji2121 <shivarkv123@gmail.com>
2026-02-18 15:48:20 +0530

    Updated the responeses

--- api.employeeService.in/src/main/java/com/employeeService/repository/EmployeeRespository.java
+++ api.employeeService.in/src/main/java/com/employeeService/repository/EmployeeRespository.java
@@ -2,6 +2,9 @@ package com.employeeService.repository;
 
 import com.employeeService.entity.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+@Repository
 public interface EmployeeRespository extends JpaRepository<EmployeeEntity,Long> {
 }
